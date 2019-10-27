04dbe27a00c06479b29d6f1c9ca6ba8ce5fefaa1
he.zhiming <he-zhiming@foxmail.com>
2019-10-27 20:01:47 +0800

    封装请求方法使用async

--- src/utils/axios_utils.js
+++ src/utils/axios_utils.js
@@ -1,61 +1,39 @@
 import axios from "axios";
 
-const _DEFAULT_HEADERS = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-}
 
-export class Axios {
-    static get(url, params) {
-        return axios.get(url,{
-            params: params,
-            headers: {
-                "Accept": "application/json",
-                ..._DEFAULT_HEADERS
-            }
-        })
-    }
+/**
+ * 使用async和await简化Promise的使用
+ *
+ * 统一异常处理
+ * 返回Promise, 所以可以使用await调用
+ *
+ *
+ * 外部使用:
+ *  const {username, password} = ajax('url')
+ */
+export function ajax(url, data = {}, type = 'GET') {
+    // 自己包装一层Promise对象
+    return new Promise((resolve, reject) => {
+        let p
+        // 执行ajax请求
+        if (type === 'GET') {
+            p = axios.get(url, {
+                params: data
+            })
+        } else {
+            p = axios.post(url, data)
+        }
 
-    /**
-     *
-     * @param url
-     * @param {Object} data
-     * @returns {Promise<AxiosResponse<T>>}
-     */
-    static post(url, data) {
-        return axios.post(url, JSON.stringify(data), {
-            headers: {
-                "Content-Type": "application/json",
-                ..._DEFAULT_HEADERS,
-            }
-        })
-    }
+        // 如果成功, resolve
+        p.then(response => {
+            resolve(response)
+        }).catch(error => { // 如果失败, reject(reason) <- 不调用, 二是提示异常信息
+           // reject(error) 不能这么做
 
-    /**
-     *
-     * @param url
-     * @param {Object} data
-     * @returns {Promise<AxiosResponse<T>>}
-     */
-    static put(url, data) {
-        return axios.put(url, JSON.stringify(data), {
-            headers: {
-                ..._DEFAULT_HEADERS,
-            }
+           // message.error("请求错误" + error.message)
         })
-    }
 
-    /**
-     *
-     * @param url
-     * @returns {Promise<AxiosResponse<T>>}
-     */
-    static delete(url) {
-        return axios.delete(url, {
-            headers: {
-                ..._DEFAULT_HEADERS,
-            }
-        })
-    }
+    })
+
+
 }
